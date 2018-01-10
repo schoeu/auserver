c8b9eb8f2ac15502c3cd8ff8803976e86600ead6
schoeu <schoeu89@gmail.com>
2018-01-10 21:30:30 +0800

    add date parameters to arrival api.

--- dataProcess/arrival.go
+++ dataProcess/arrival.go
@@ -4,17 +4,29 @@ import (
 	"database/sql"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"time"
 
 	"../autils"
-	"time"
+	"regexp"
 )
 
 // 组件概况数据处理
 func UpdateArrival(c *gin.Context, db *sql.DB) {
+	dateReg := regexp.MustCompile("(\\d{4})(\\d{2})(\\d{2})")
 	data := c.Query("data")
+	date := c.Query("date")
 
 	count := autils.CheckSql(data)
-	date := autils.GetCurrentData(time.Now().AddDate(0, 0, -1))
+
+	if date == "" {
+		date = autils.GetCurrentData(time.Now().AddDate(0, 0, -1))
+	} else {
+		dateArr := dateReg.FindAllStringSubmatch(date, -1)
+		if len(dateArr) > 0 && len(dateArr[0]) > 3 {
+			year, month, day := dateArr[0][1], dateArr[0][2], dateArr[0][3]
+			date = year + "-" + month + "-" + day
+		}
+	}
 
 	rsText := "ok"
 	_, err := db.Exec("update all_flow set wb_pv =  '" + count + "' where date = '" + date + "'")
