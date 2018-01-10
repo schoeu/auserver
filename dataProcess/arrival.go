e56145aa59630dad867a082c857d2db14052f6a4
schoeu <schoeu89@gmail.com>
2018-01-10 17:28:09 +0800

    add arrival insert.

--- dataProcess/arrival.go
+++ dataProcess/arrival.go
@@ -2,18 +2,29 @@ package dataProcess
 
 import (
 	"database/sql"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"../autils"
+	"time"
 )
 
 // 组件概况数据处理
 func UpdateArrival(c *gin.Context, db *sql.DB) {
 	data := c.Query("data")
-	fmt.Println(data)
+
+	count := autils.CheckSql(data)
+	date := autils.GetCurrentData(time.Now().AddDate(0, 0, -1))
+
+	rsText := "ok"
+	_, err := db.Exec("update all_flow set wb_pv =  '" + count + "' where date = '" + date + "'")
+
+	if err != nil {
+		rsText = "failed"
+	}
+
 	c.JSON(http.StatusOK, gin.H{
 		"status": 0,
-		"msg":    "ok",
-		"data":   data,
+		"msg":    rsText,
 	})
 }
