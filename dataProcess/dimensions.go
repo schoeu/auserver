4b0147f8d15c625a457ef81b43b2a0bf86b34702
schoeu <schoeu89@gmail.com>
2018-02-07 19:27:22 +0800

    add mutiple page.

--- dataProcess/dimensions.go
+++ dataProcess/dimensions.go
@@ -1,9 +1,11 @@
 package dataProcess
 
 import (
+	"bytes"
 	"database/sql"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 	"time"
 
 	"../autils"
@@ -18,12 +20,16 @@ type dimRowsInfo struct {
 type dimData struct {
 	Columns []tStruct     `json:"columns"`
 	Rows    []dimRowsInfo `json:"rows"`
+	Total   int           `json:"total"`
 }
 
 // 作弊请求数据处理
 func Dimensions(c *gin.Context, db *sql.DB) {
 	position := "left"
 	cd := dimData{}
+
+	start := c.Query("start")
+	limit := c.Query("limit")
 
 	date := c.Query("date")
 	if date == "" {
@@ -50,9 +56,10 @@ func Dimensions(c *gin.Context, db *sql.DB) {
 		position,
 	}}
 
-	infos := getDimInfo(db, s)
+	infos, sum := getDimInfo(db, s, start, limit)
 
 	cd.Rows = infos
+	cd.Total = sum
 
 	c.JSON(http.StatusOK, gin.H{
 		"status": 0,
@@ -61,14 +68,27 @@ func Dimensions(c *gin.Context, db *sql.DB) {
 	})
 }
 
-func getDimInfo(db *sql.DB, date string) []dimRowsInfo {
+func getDimInfo(db *sql.DB, date, start, limit string) ([]dimRowsInfo, int) {
 	showText := []string{"", "二跳", "多跳"}
-	sqlStr := "select type, url, count from mip_step where date = '" + date + "' order by count desc"
-	rows, err := db.Query(sqlStr)
+
+	var sqlStr bytes.Buffer
+	sqlStr.WriteString("select type, url, count from mip_step where date = '" + date + "' order by count desc ")
+
+	_, err := strconv.Atoi(limit)
+	if err == nil {
+		sqlStr.WriteString(" limit " + limit + "")
+	}
+
+	_, err = strconv.Atoi(start)
+	if err == nil {
+		sqlStr.WriteString(" offset " + start + "")
+	}
+
+	rows, err := db.Query(sqlStr.String())
 	autils.ErrHadle(err)
 
 	var url string
-	var count, dType int
+	var count, dType, sum int
 	cri := dimRowsInfo{}
 	criArr := []dimRowsInfo{}
 	for rows.Next() {
@@ -78,10 +98,11 @@ func getDimInfo(db *sql.DB, date string) []dimRowsInfo {
 		cri.Num = count
 		cri.MType = showText[dType]
 		criArr = append(criArr, cri)
+		sum++
 	}
 	err = rows.Err()
 	autils.ErrHadle(err)
 
 	defer rows.Close()
-	return criArr
+	return criArr, sum
 }
